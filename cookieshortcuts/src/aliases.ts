f7c247f822112122591b2c02bdc491ef3b044861
MastarCheeze <mon10180218@gmail.com>
2024-03-08 18:19:12 +0800

    disable debugs

--- cookieshortcuts/src/aliases.ts
+++ cookieshortcuts/src/aliases.ts
@@ -21,29 +21,29 @@ const modsReadyInterval = setInterval(() => {
 const gardenInterval = setInterval(() => {
     if (Game.Objects["Farm"]["minigameLoaded"]) {
         Garden = Game.Objects["Farm"]["minigame"];
-        w.Garden = Garden; // DEBUG
+        // w.Garden = Garden; // DEBUG
         clearInterval(gardenInterval);
     }
 }, 1000);
 const marketInterval = setInterval(() => {
     if (Game.Objects["Bank"]["minigameLoaded"]) {
         Market = Game.Objects["Bank"]["minigame"];
-        w.Market = Market; // DEBUG
-        w.Market.secondsPerTick = 1; // DEBUG
+        // w.Market = Market; // DEBUG
+        // w.Market.secondsPerTick = 1; // DEBUG
         clearInterval(marketInterval);
     }
 }, 1000);
 const pantheonInterval = setInterval(() => {
     if (Game.Objects["Temple"]["minigameLoaded"]) {
         Pantheon = Game.Objects["Temple"]["minigame"];
-        w.Pantheon = Pantheon; // DEBUG
+        // w.Pantheon = Pantheon; // DEBUG
         clearInterval(pantheonInterval);
     }
 }, 1000);
 const grimoireInterval = setInterval(() => {
     if (Game.Objects["Wizard tower"]["minigameLoaded"]) {
         Grimoire = Game.Objects["Wizard tower"]["minigame"];
-        w.Grimoire = Grimoire; // DEBUG
+        // w.Grimoire = Grimoire; // DEBUG
         clearInterval(grimoireInterval);
     }
 }, 1000);
