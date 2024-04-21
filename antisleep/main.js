e983bfd2420235796f2301673a0acc3a5c729466
MastarCheeze <mon10180218@gmail.com>
2024-04-21 20:41:12 +0800

    add loaded notification

--- antisleep/main.js
+++ antisleep/main.js
@@ -2,3 +2,5 @@ let audio = new Audio("https://adventure.land/sounds/loops/empty_loop_for_js_per
 setInterval(() => {
     audio.play();
 }, 30000);
+
+Game.Notify("Antisleep loaded!", "", null, true);
