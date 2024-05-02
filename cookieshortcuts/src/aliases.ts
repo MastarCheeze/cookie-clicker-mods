0a3d24cd60773e2096805d37f5d3a0d68c59a647
MastarCheeze <mon10180218@gmail.com>
2024-05-02 19:33:54 +0800

    remove unused code

--- cookieshortcuts/src/aliases.ts
+++ cookieshortcuts/src/aliases.ts
@@ -7,16 +7,6 @@ export let Market: any;
 export let Pantheon: any;
 export let Grimoire: any;
 
-let modsReady = false;
-
-const modsReadyInterval = setInterval(() => {
-    for (const mod of Object.values(Game.mods)) {
-        if ((mod as any).init !== 0) return;
-    }
-    modsReady = true;
-    clearInterval(modsReadyInterval);
-}, 100);
-
 // initialise shortened minigame variables
 const gardenInterval = setInterval(() => {
     if (Game.Objects["Farm"]["minigameLoaded"]) {
