e05c2217742470294ee4f53cd1e12dde277cf27c
MastarCheeze <mon10180218@gmail.com>
2024-05-07 22:30:50 +0800

    add stock icon to messages

--- misc/market-notifications.js
+++ misc/market-notifications.js
@@ -39,7 +39,27 @@ const MarketNotifications = {
     }
 
     function main() {
-        let Market = Game.Objects.Bank.minigame;
+        const Market = Game.Objects.Bank.minigame;
+        const symbolToIcon = {
+            CRL: [2, 33],
+            CHC: [3, 33],
+            BTR: [4, 33],
+            SUG: [15, 33],
+            NUT: [16, 33],
+            SLT: [17, 33],
+            VNL: [5, 33],
+            EGG: [6, 33],
+            CNM: [7, 33],
+            CRM: [8, 33],
+            JAM: [13, 33],
+            WCH: [14, 33],
+            HNY: [19, 33],
+            CKI: [20, 33],
+            RCP: [32, 33],
+            SBD: [33, 33],
+            PBL: [34, 33],
+            YOU: [35, 33],
+        };
 
         Market.tick = (function (old) {
             return (...params) => {
@@ -51,35 +71,45 @@ const MarketNotifications = {
 
                 let notify = false;
 
-                let toBuy = Object.values(Market.goods).filter(
-                    (stock) => stock.val <= 3 && stock.stock < Market.getGoodMaxStock(stock),
-                );
-                let toBuyNames = toBuy.map((stock) => stock.symbol);
+                let toBuy = Object.values(Market.goods)
+                    .filter((stock) => stock.val <= 3 && stock.stock < Market.getGoodMaxStock(stock))
+                    .map((stock) => stock.symbol);
                 if (MarketNotifications.toBuy && toBuy.length > 0) {
                     notify = true;
+
+                    for (symbol of toBuy) {
+                        Game.Notify("Prices are low!", `${symbol} is below $3`, symbolToIcon[symbol], 60);
+                    }
+
                     let text = "";
                     if (toBuy.length > 1) {
-                        text += `${toBuyNames.slice(0, -1).join(", ")} and `;
+                        text += `${toBuy.slice(0, -1).join(", ")} and `;
                     }
-                    text += `${toBuyNames.slice(-1)[0]} is below $3`;
-                    Game.Notify("Prices are low!", text, null, 50);
+                    text += toBuy.slice(-1)[0];
+                    text += toBuy.length === 1 ? " is below $3" : " are below $3";
                     createNotification("Prices are low!", text);
                 }
 
-                let toSell = Object.values(Market.goods).filter(
-                    (stock) =>
-                        stock.val >= (130 / 17) * Market.goodsById.indexOf(stock) + 49 + Game.Objects.Bank.level &&
-                        stock.stock > 0,
-                );
-                let toSellNames = toSell.map((stock) => stock.symbol);
+                let toSell = Object.values(Market.goods)
+                    .filter(
+                        (stock) =>
+                            stock.val >= (130 / 17) * Market.goodsById.indexOf(stock) + 49 + Game.Objects.Bank.level &&
+                            stock.stock > 0,
+                    )
+                    .map((stock) => stock.symbol);
                 if (MarketNotifications.toSell && toSell.length > 0) {
                     notify = true;
+
+                    for (symbol of toSell) {
+                        Game.Notify("Prices are high!", `${symbol} is ready to sell`, symbolToIcon[symbol], 60);
+                    }
+
                     let text = "";
                     if (toSell.length > 1) {
-                        text += `${toSellNames.slice(0, -1).join(", ")} and `;
+                        text += `${toSell.slice(0, -1).join(", ")} and `;
                     }
-                    text += `${toSellNames.slice(-1)[0]} is ready to sell`;
-                    Game.Notify("Prices are high!", text, null, 50);
+                    text += toSell.slice(-1)[0];
+                    text += toSell.length === 1 ? " is ready to sell" : " are ready to sell";
                     createNotification("Prices are high!", text);
                 }
 
