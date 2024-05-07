f759d0987b638a7dd336c9ddf4fc5184b9d3661a
MastarCheeze <mon10180218@gmail.com>
2024-05-07 23:01:24 +0800

    better tooltips

--- misc/cyclius-info.js
+++ misc/cyclius-info.js
@@ -5,20 +5,47 @@
 
     Object.defineProperty(Pantheon.gods.ages, "desc1", {
         get: () => {
-            const bonus = 0.15 * Math.sin((Date.now() / 1000 / (60 * 60 * 3)) * Math.PI * 2) * 100;
-            return `Effect cycles over 3 hours. Currently at ${bonus > 0 ? "+" : ""}${bonus.toFixed(2)}%`;
+            const bonus = 15 * Math.sin((Date.now() / 1000 / (60 * 60 * 3)) * Math.PI * 2);
+            const increasing = Math.cos((Date.now() / 1000 / (60 * 60 * 3)) * Math.PI * 2) >= 0;
+
+            let text = "Effect cycles over 3 hours. Currently at ";
+            text += `<span class="${bonus >= 0 ? "green" : "red"}">`;
+            text += `${bonus > 0 ? "+" : ""}${bonus.toFixed(2)}%`;
+            text += "</span>";
+            text += `<span class="${increasing ? "green" : "red"}">`;
+            text += increasing ? "▲" : "▼";
+            text += "</span>";
+            return text;
         },
     });
     Object.defineProperty(Pantheon.gods.ages, "desc2", {
         get: () => {
-            const bonus = 0.15 * Math.sin((Date.now() / 1000 / (60 * 60 * 12)) * Math.PI * 2) * 100;
-            return `Effect cycles over 12 hours. Currently at ${bonus > 0 ? "+" : ""}${bonus.toFixed(2)}%`;
+            const bonus = 15 * Math.sin((Date.now() / 1000 / (60 * 60 * 12)) * Math.PI * 2);
+            const increasing = Math.cos((Date.now() / 1000 / (60 * 60 * 12)) * Math.PI * 2) >= 0;
+
+            let text = "Effect cycles over 12 hours. Currently at ";
+            text += `<span class="${bonus >= 0 ? "green" : "red"}">`;
+            text += `${bonus > 0 ? "+" : ""}${bonus.toFixed(2)}%`;
+            text += "</span>";
+            text += `<span class="${increasing ? "green" : "red"}">`;
+            text += increasing ? "▲" : "▼";
+            text += "</span>";
+            return text;
         },
     });
     Object.defineProperty(Pantheon.gods.ages, "desc3", {
         get: () => {
-            const bonus = 0.15 * Math.sin((Date.now() / 1000 / (60 * 60 * 24)) * Math.PI * 2) * 100;
-            return `Effect cycles over 24 hours. Currently at ${bonus > 0 ? "+" : ""}${bonus.toFixed(2)}%`;
+            const bonus = 15 * Math.sin((Date.now() / 1000 / (60 * 60 * 24)) * Math.PI * 2);
+            const increasing = Math.cos((Date.now() / 1000 / (60 * 60 * 24)) * Math.PI * 2) >= 0;
+
+            let text = "Effect cycles over 24 hours. Currently at ";
+            text += `<span class="${bonus >= 0 ? "green" : "red"}">`;
+            text += `${bonus > 0 ? "+" : ""}${bonus.toFixed(2)}%`;
+            text += "</span>";
+            text += `<span class="${increasing ? "green" : "red"}">`;
+            text += increasing ? "▲" : "▼";
+            text += "</span>";
+            return text;
         },
     });
 })();
